b3d9ebe7780593e91c3f21f65a92ab33c14a1e9e
Dao Thanh Hai <thanhhai.ittalk@gmail.com>
2018-07-30 10:05:17 +0700

    updater

---
+++ main/updater.c
@@ -0,0 +1,8 @@
+/*
+ * updater.c
+ *
+ *  Created on: Jul 30, 2018
+ *      Author: hai_dotcom
+ */
+
+#include "updater.h"
---
+++ main/updater.h
@@ -0,0 +1,13 @@
+/*
+ * updater.h
+ *
+ *  Created on: Jul 30, 2018
+ *      Author: hai_dotcom
+ */
+
+#ifndef MAIN_UPDATER_H_
+#define MAIN_UPDATER_H_
+
+
+
+#endif /* MAIN_UPDATER_H_ */
